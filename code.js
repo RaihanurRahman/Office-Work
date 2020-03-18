100c5437ea44c731485671c161d0a89f5e1eb0db
Raihanur Rahman <raihanur.rahman@selise.com.bd>
2020-03-18 18:51:24 +0600

    New Project

---
+++ code.js
@@ -0,0 +1,6 @@
+function change() {
+	var rangeLabel = document.getElementById("range-label");
+	var experience = document.getElementById("experience");
+	rangeLabel.innerText = experience.value + "K";
+}
+
